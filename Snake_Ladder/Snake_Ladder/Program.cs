75bdababcf7a7dd590a1a54369493d6774310e6f
Rajangane <pratiksharajangane7@gmail.com>
2021-11-29 20:44:21 +0530

    [Pratiksha] ADD : Reporting number of times the dice was played

--- Snake_Ladder/Snake_Ladder/Program.cs
+++ Snake_Ladder/Snake_Ladder/Program.cs
@@ -5,4 +5,5 @@ using Snake_Ladder;
 //new UC2_snake().DiceRoll();
 //new UC3_Snake().PositionCheck();
 //new UC4_Snake().WinnerPosition();
-new UC5_Snake().ExactWinning();
+//new UC5_Snake().ExactWinning();
+new UC6_Snake().TimesPlayed();
---
+++ Snake_Ladder/Snake_Ladder/UC6_Snake.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_Ladder
+{
+    internal class UC6_Snake
+    {
+        public static int PlayerPosition = 0;
+        const int No_Play = 0;
+        const int ladder = 1;
+        const int snake = 2;
+        public void TimesPlayed()
+        {
+            int counter = 0;
+            int exactwinningposition = 100;
+            Random ranval = new Random();
+            while (PlayerPosition < exactwinningposition)
+            {
+                while (PlayerPosition != exactwinningposition)
+                {
+                    int diceroll = ranval.Next(1, 7);
+                    counter++;
+                    int check_play = ranval.Next(3);
+                    {
+                        switch (check_play)
+                        {
+                            case 1:
+                                PlayerPosition += diceroll;
+                                if (PlayerPosition > exactwinningposition)
+                                {
+                                    PlayerPosition = exactwinningposition;
+                                    PlayerPosition -= diceroll;
+                                }
+                                break;
+                            case 2:
+                                PlayerPosition -= diceroll;
+                                if (PlayerPosition < 0)
+                                {
+                                    PlayerPosition = 0;
+                                }
+                                break;
+                            case 0:
+                                break;
+                        }
+                    }
+                }
+                Console.WriteLine("U arrived at the Finish Point: " + PlayerPosition);
+                Console.WriteLine("The number of times the dice played is: " + counter);
+            }
+        }
+    }
+}
