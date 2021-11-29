6e3fad864d893d32f97be370411396603fd0987c
Rajangane <pratiksharajangane7@gmail.com>
2021-11-29 19:36:29 +0530

    [Pratiksha] ADD : Player rolls the Die

--- Snake_Ladder/Snake_Ladder/Program.cs
+++ Snake_Ladder/Snake_Ladder/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("************ Snake & Ladder ****************");
 using Snake_Ladder;
-new UC1_Snake().Position();
+//new UC1_Snake().Position();
+new UC2_snake().DiceRoll();
---
+++ Snake_Ladder/Snake_Ladder/UC2_snake.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_Ladder
+{
+    internal class UC2_snake
+    {
+        public  void DiceRoll()
+        {
+            int player1;
+            Random rd = new Random();
+            player1 = rd.Next(1, 7);
+            Console.WriteLine("The player1 gets the dice number is: " + player1);
+
+        }
+
+    }
+}
